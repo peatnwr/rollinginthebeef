45d138557a028e275f1c9ec6788aec91dd110ebb
peatnwr <nwrpeat@gmail.com>
2021-10-10 19:44:26 +0700

    implement order page

--- app/src/main/java/com/example/rollinginthebeef/modules/OrderAdapter.kt
+++ app/src/main/java/com/example/rollinginthebeef/modules/OrderAdapter.kt
@@ -10,15 +10,15 @@ import com.example.rollinginthebeef.databinding.OrderItemLayoutBinding
 class OrderAdapter(val orderList: ArrayList<Order>?, val context: Context)
     : RecyclerView.Adapter<OrderAdapter.ViewHolder>(){
     inner class ViewHolder(view: View, val binding: OrderItemLayoutBinding)
-        : RecyclerView.ViewHolder(view){
+        : RecyclerView.ViewHolder(view) {
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = OrderItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding.root, binding)
     }
 
-    override fun onBindViewHolder(holder: OrderAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val binding = holder.binding
         binding.txOrderID.text = orderList!![position].order_id.toString()
         binding.txCustomer.text = orderList!![position].user_username
